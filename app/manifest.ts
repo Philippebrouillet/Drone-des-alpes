d64d174570c802d2f08a955a8793ac3ef56bcf34
Philippe Brouillet <philippebrouillet@icloud.com>
2025-12-18 09:52:45 +0100

    feat: update favicon

--- app/manifest.ts
+++ app/manifest.ts
@@ -13,9 +13,16 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#0070f3",
     icons: [
       {
-        src: "/logo.jpg",
-        sizes: "any",
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
         type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
   };
